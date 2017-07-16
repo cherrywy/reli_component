23753c0e2df1ee886ae806de84c577c605a9ba5a
ying <wangying@geekniu.com>
2017-07-16 21:45:04 +0800

    商品上架分页功能和数据导入数据展示完成

--- src/views/goodsMgt/goodsMgtDataImport.vue
+++ src/views/goodsMgt/goodsMgtDataImport.vue
@@ -1,14 +1,20 @@
 <template>
     <section>
         <el-col :span="24" style="margin-bottom:20px">
-             <el-upload class="avatar-uploader" ref="upload" style="float:right;margin-right:20px;" action="http://118.89.232.160:10001/util/file/upload.json" name="pic" :data="{type:'file'}" :show-file-list="false">
-                      <el-button type="primary" style="background:#70a5ec"  @click="handleAvatarSuccess">导入销售数据</el-button>      
+             <el-upload class="avatar-uploader" ref="upload" style="float:right;margin-right:20px;"  action="http://118.89.232.160:10001/a/goods/sales.json" :data="{'uid': 1209811640320002,'shop_id':1272219830442436,'header_index':4,'file':fileList.name}" :file-list="fileList" :on-success="handleImportSuccess" :show-file-list="false">
+                      <el-button type="primary" style="background:#70a5ec"  >导入销售数据</el-button>      
             </el-upload>
+        </el-col>
+        <el-col :span="24" style="margin-bottom:20px">
+            <div v-for='item in dataImprt'  style="border-bottom: 1px solid #ccc;padding:10px;">
+                <span>{{item.data.goods_name}}</span>
+                <el-button type='text'style="float:right;margin-right:50px">搜索</el-button>
+            </div>
         </el-col>
         <el-dialog :visible.sync="dialogFormVisible" >
                     <el-form style='text-align: center;'>
                         <el-form-item label="">
-                            <el-input style='width:200px;float:right;'v-model="goodsName" placeholder="请输入商品名称搜索" icon='search' :on-icon-click="searchGoodsName" auto-complete="off"></el-input>
+                            <el-input style='width:200px;float:right;'v-model="goodsName" placeholder="请输入商品名称搜索" icon='search' :file-list="fileList" :on-icon-click="searchGoodsName" auto-complete="off"></el-input>
                         </el-form-item>
                        <p style='color:red;margin:50px auto 100px;text-algin:center'>未搜索到该商品</p>
                          <el-button type="primary" @click="submit" style="margin:auto 0; width:20%;">绑定</el-button>
@@ -20,13 +26,17 @@
 </template>
 <script>
 
-import { requestUpload } from '../../api/goodsServer';
+import { requestUpload,requestDataImport } from '../../api/goodsServer';
 import moment from 'moment';
 export default {
     data() {
         return {
+            dataImprt:[],
             header_index:3,
-             dialogFormVisible: true,
+            goodsName:'',
+            fileList:[],
+            dialogFormVisible: false,
+            
               
         }
     },
@@ -34,23 +44,20 @@ export default {
        
     },
     methods: {
-         handleAvatarSuccess(res, file) {
-            const fileUrl = res.result.file_download_url
-            let brandParams = { uid: 1209812865056771, file: fileUrl,header_index:this.header_index,shop_id:this.shop_id };
-            requestBrandHistory(brandParams).then(data => {
-                let { error_code, result } = data;
-                if (error_code !== 0) {
+        handleImportSuccess(res, fileList) {
+            if (res.result.error_code !== 0) {
                     this.$message({
-                        message: "返回数据有误",
-                        type: 'error'
+                        message: "导入数据成功",
+                        type: 'success'
                     });
-                } else {
-                    this.brand = result.list
+            this.dataImprt=res.result
                 }
-            })
+        
         },
         
+     searchGoodsName(){
 
+     }
     }
 }
 </script>
--- src/views/goodsMgt/goodsMgtOnline.vue
+++ src/views/goodsMgt/goodsMgtOnline.vue
@@ -1,15 +1,15 @@
 <template>
     <section>
         <el-row :gutter="20">
-            <el-form :inline="true"  class="demo-form-inline" style="margin-left: 10px;">
+            <el-form :inline="true" class="demo-form-inline" style="margin-left: 10px;">
                 <el-form-item label="请选择门店：">
-                    <el-select v-model="shopName" placeholder="请选择">
+                    <el-select v-model="shopName" placeholder="请选择" @change="getFindShopPlan()">
                         <el-option v-for="item in shop" :key="item.value" :label="item.label" :value="item.value">
                         </el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="请选择布局图：">
-                    <el-select v-model="plansName" placeholder="请选择">
+                    <el-select v-model="plansName" placeholder="请选择" @change='getPlanShowCaseList()'>
                         <el-option v-for="item in shopPlan" :key="item.value" :label="item.label" :value="item.value">
                         </el-option>
                     </el-select>
@@ -17,13 +17,13 @@
             </el-form>
         </el-row>
         <el-row :gutter="20" :offset="10" style="margin-bottom:20px;text-align:center;">
-            <el-card style="margin: 0 auto; width:500px;height:300px;">
-                <img src="" class="image">
-                <div style="padding: 14px;">
-                    <span>好吃的汉堡</span>
-                    <div class="bottom clearfix">
-                        <time class="time"></time>
-                        <el-button type="text" class="button">操作按钮</el-button>
+            <el-card style="margin: 0 auto; width:500px;height:300px;position:relative">
+                <div class="canvas" ref="canvas">
+                    <canvas id="stage"></canvas>
+                </div>
+                <div style="bottom: 0;position: absolute;">
+                    <div class="bottom">
+                        <el-button type="text" class="button">展柜数量:{{planShowCase.show_case_type}}</el-button>
                     </div>
                 </div>
             </el-card>
@@ -31,29 +31,28 @@
         <el-card class="box-card">
             <div slot="header" class="clearfix">
                 <span style="line-height: 36px;">卡片名称</span>
-                <el-button style="float: right;" type="primary" @click="getOnlineList()">上架商品</el-button>
+                <el-button style="float: right;" type="primary" @click="getOnlineList('', 0, 5)">上架商品</el-button>
                 <el-dialog :visible.sync="dialogFormVisible">
                     <el-form>
                         <el-form-item label="">
                             <el-input v-model="goodsName" placeholder="请输入商品名称搜索" icon='search' :on-icon-click="searchGoodsName" auto-complete="off"></el-input>
                         </el-form-item>
-                        <el-button type="text" style="float:right" @click="goodsNew()"><span style="color:#333">找不到商品？去</span>添加</el-button 
-                        <el-form-item>
-                            <el-table ref="multipleTable" :data="tableOnlineList" border tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
-                                <el-table-column type="selection" width="55">
-                                </el-table-column>
-                                <el-table-column prop='goods_name' label="商品名称">
-                                </el-table-column>
-                                <el-table-column  label="商品规格">
-                                     <template scope="scope">
-                                     {{scope.row.spec1_value?scope.row.spec1_value:''}} {{scope.row.spec2_value?","+scope.row.spec2_value:''}}{{scope.row.spec3_value?","+scope.row.spec3_value:''}}
-                                     </template>
-                                </el-table-column>
-                            </el-table>
-                            <el-row class="row center">
-                                 <el-pagination style="margin: 20px auto;float:right" @size-change="handleSizeChange"  @current-change="handleCurrentChange" :current-page="currentPage"
-                                :page-sizes="[5, 10, 15, 20]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
-                            </el-row>
+                        <el-button type="text" style="float:right" @click="goodsNew()">
+                            <span style="color:#333">找不到商品？去</span>添加</el-button <el-form-item>
+                        <el-table ref="multipleTable" :data="tableOnlineList" border tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+                            <el-table-column type="selection" width="55">
+                            </el-table-column>
+                            <el-table-column prop='goods_name' label="商品名称">
+                            </el-table-column>
+                            <el-table-column label="商品规格">
+                                <template scope="scope">
+                                    {{scope.row.spec1_value?scope.row.spec1_value:''}} {{scope.row.spec2_value?","+scope.row.spec2_value:''}}{{scope.row.spec3_value?","+scope.row.spec3_value:''}}
+                                </template>
+                            </el-table-column>
+                        </el-table>
+                        <el-row class="row center">
+                            <el-pagination style="margin: 20px auto;float:right" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[5, 10, 15, 20]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
+                        </el-row>
                         </el-form-item>
                     </el-form>
                     <div style="text-align:center">
@@ -74,43 +73,92 @@
                     </template>
                 </el-table-column>
             </el-table>
-            <!--<el-row class="row center">
-                           <el-pagination style="margin: 20px auto;float:right" @current-change="handleCurrentChange" :current-page="currentPage" :page-size="10" layout="total, prev, pager, next, jumper" :total='total'>
-                           </el-pagination>
-             </el-row>-->
         </el-card>
     
     </section>
 </template>
 <script>
-import { requestOnline, requestFindShopPlan, requestSearchSpec, requestOnlineShowCase, requestPlanShowCaseList,requestGoodsOnline,requestGoodsOffline} from '../../api/goodsServer';
+import { requestOnline, requestFindShop, requestFindShopPlan, requestSearchSpec, requestOnlineShowCase, requestPlanShowCaseList, requestGoodsOnline, requestGoodsOffline } from '../../api/goodsServer';
 export default {
     data() {
         return {
+            canvas: null,
+            coorDefaults: {
+                originX: 'center',
+                originY: 'center',
+                fill: 'rgba(66, 134, 244, 0.7)',
+                stroke: 'rgba(255, 255, 255, 1)',
+                strokeWidth: 1,
+                scaleX: 1,
+                scaleY: 1,
+                flipX: false,
+                flipY: false,
+                opacity: 1,
+                visible: true,
+                fillRule: 'nonzero',
+                hasBorders: false,
+                hasControls: false,
+                hasRotatingPoint: false,
+                lockMovementX: true,
+                lockMovementY: true,
+                lockScalingX: true,
+                lockScalingY: true,
+                lockUniScaling: true,
+                lockRotation: true
+            },
             shop: [],
             shopPlan: [],
             shopName: '',
             plansName: '',
             tableOnlineList: [],
+            planShowCase: [],
             tableData: [],
             goodsName: '',
             currentPage: 1,
-            pageSize: 10,
-            total: 20,
+            pageSize: 5,
+            total: 0,
             multipleSelection: [],
             dialogFormVisible: false,
             formLabelWidth: '120px',
-            
-           
         }
     },
     mounted: function () {
-        this.getFindShopPlan()//获取门店
-        this.getPlanShowCaseList()//获取平面图
+        this.getFindShop()//获取门店
+    },
+    wach: {
+        canvas() {
+            this.renderObjects()
+        },
     },
     methods: {
-        getOnlineShowCaseList(){
-            let OnlineListParams = {show_case_id:show_case_id};
+        renderObjects() {
+            // 绘制接口中获取的形状
+            if (this.canvas) {
+                this.planShowCase.forEach( shape => {
+                    let coord = _.merge(shape.coord, this.coorDefaults)
+                    let object = {}
+                    if (coord.type === 'rect' || coord.type === 0) {
+                        object = new window.fabric.Rect(coord)
+                    } else if (coord.type === 'circle' || coord.type === 1) {
+                        object = new window.fabric.Circle(coord)
+                    } else {
+                        return false
+                    } 
+                    object.set('fill', )
+                    object.set('id', shape.data.id)
+                    object.set('data', {
+                        name:shape.data.name,
+                        plan_id: shape.data.plan_id,
+                        type: shape.data.show_case_type,
+                        sku_group: shape.data.sku_group_ids
+                    })
+                    this.canvas.add(object)
+                })
+               
+            }
+        },
+        getOnlineShowCaseList() {
+            let OnlineListParams = { show_case_id: show_case_id };
             requestOnlineShowCase(OnlineListParams).then(data => {
                 let { error_code, result } = data;
                 if (error_code !== 0) {
@@ -119,18 +167,18 @@ export default {
                         type: 'error'
                     });
                 } else {
-                   
+
                 }
             })
         },
-        
-        handleOffline(index, goods_spec_id){
+
+        handleOffline(index, goods_spec_id) {
             this.$confirm('下架该商品后将无法撤回，是否继续', '提示', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
                 type: 'warning'
             }).then(() => {
-                let removeParams = { goods_spec_id: goods_spec_id,show_case_id:show_case_id};
+                let removeParams = { goods_spec_id: goods_spec_id, show_case_id: show_case_id };
                 requestGoodsOffline(removeParams).then(data => {
                     let { error_code, result } = data;
                     if (error_code == 0) {
@@ -150,11 +198,11 @@ export default {
                 });
             });
         },
-        submit(){
-             const goods_spec_ids=this.multipleSelection.map(v =>{
-             return v.id
-         }).join()
-             let listParams = { uid: 1209811640320002, goods_spec_ids:goods_spec_ids,show_case_id:show_case_id};
+        submit() {
+            const goods_spec_ids = this.multipleSelection.map(v => {
+                return v.id
+            }).join()
+            let listParams = { uid: 1209811640320002, goods_spec_ids: goods_spec_ids, show_case_id: show_case_id };
             requestGoodsOnline(listParams).then(data => {
                 let { error_code, result } = data;
                 if (error_code !== 0) {
@@ -167,14 +215,18 @@ export default {
                         message: "保存成功",
                         type: 'success'
                     });
-                  dialogFormVisible = false;
+                    dialogFormVisible = false;
                 }
             })
-            
+
         },
-        getPlanShowCaseList(plan_id) {
-            let listParams = { uid: 1209811640320002, plan_id: plan_id };
-            requestPlanShowCaseList(listParams).then(data => {
+        getPlanShowCaseList() {
+            const plan_id = this.shopPlan.filter(v => {
+                return v.value === this.plansName;
+            }).map(v => v.id).pop();
+
+            let planListParams = { plan_id: plan_id };
+            requestPlanShowCaseList(planListParams).then(data => {
                 let { error_code, result } = data;
                 if (error_code !== 0) {
                     this.$message({
@@ -182,18 +234,19 @@ export default {
                         type: 'error'
                     });
                 } else {
-
+                    alert(plan_id)
+                    this.planShowCase = result
                 }
             })
         },
-        getOnlineList(key_word, pageNum,limit) {
+        getOnlineList(key_word, pageNum, limit) {
             this.dialogFormVisible = true;
-            let listParams = { uid: 1209811640320002, page: pageNum ,limit:limit};
+            let listParams = { uid: 1209811640320002, page: pageNum, limit: limit };
             if (key_word) {
                 listParams['key_word'] = key_word
             }
             requestSearchSpec(listParams).then(data => {
-                let { error_code, result } = data;
+                let { error_code, result ,total_count} = data;
                 if (error_code !== 0) {
                     this.$message({
                         message: "返回数据有误",
@@ -201,25 +254,25 @@ export default {
                     });
                 } else {
                     this.tableOnlineList = result
-                    // this.total = result.total_count;
+                    this.total = total_count;
                 }
             })
         },
         searchGoodsName() {
-            this.getOnlineList(this.goodsName, 0,this.pageSize)
+            this.getOnlineList(this.goodsName, 0, this.pageSize)
             this.key_word = ''
         },
         handleCurrentChange(val) {
             this.currentPage = val;
-            this.getOnlineList(this.plansName, this.currentPage,this.pageSize)
+            this.getOnlineList(this.plansName, this.currentPage, this.pageSize)
         },
-        handleSizeChange(val){
-            this.pageSize= val;
-            this.getOnlineList(this.plansName, this.currentPage,this.pageSize)
+        handleSizeChange(val) {
+            this.pageSize = val;
+            this.getOnlineList(this.plansName, this.currentPage, this.pageSize)
         },
-        getFindShopPlan() {
+        getFindShop() {
             const shopPlanParams = { uid: 1209811640320002 };
-            requestFindShopPlan(shopPlanParams).then(data => {
+            requestFindShop(shopPlanParams).then(data => {
                 let { error_code, result } = data;
                 if (error_code !== 0) {
                     this.$message({
@@ -229,20 +282,41 @@ export default {
                 } else {
                     this.shop = result.map(v => {
                         return {
-                            value: v.shop
+                            value: v.shop,
+                            id: v.id,
 
                         }
                     });
-                    //  const shop_id = result.filter(v => {
-                    //       return v.shop === this.shopName;
-                    //   }).map(v => v.id).pop();
-                    //   alert(this.shopName)
+
                 }
             })
         },
-      handleSelectionChange(val) {
-         this.multipleSelection = val;
-      },
+        getFindShopPlan() {
+            const shop_id = this.shop.filter(v => {
+                return v.value === this.shopName;
+            }).map(v => v.id).pop();
+            const shopPlanParams = { uid: 1209811640320002, shop_id: shop_id };
+            requestFindShopPlan(shopPlanParams).then(data => {
+                let { error_code, result } = data;
+                if (error_code !== 0) {
+                    this.$message({
+                        message: "返回数据有误",
+                        type: 'error'
+                    });
+                } else {
+                    this.shopPlan = result.map(v => {
+                        return {
+                            value: v.name,
+                            id: v.id,
+
+                        }
+                    });
+                }
+            })
+        },
+        handleSelectionChange(val) {
+            this.multipleSelection = val;
+        },
         goodsNew() {
             const path = '/goodsMgtNew';
             this.$router.push({ path: path });
@@ -261,15 +335,5 @@ export default {
 .image {
     width: 100%;
     display: block;
-}
-
-.clearfix:before,
-.clearfix:after {
-    display: table;
-    content: "";
-}
-
-.clearfix:after {
-    clear: both
 }
 </style>
